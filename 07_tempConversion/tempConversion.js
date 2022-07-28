fc5fb5f9ebe1c60cc392927836912ade2edfe360
Rimon Moomey <rimon.moomey@gmail.com>
2022-07-28 16:40:50 -0500

    Complete exercise 7.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,15 @@
-const ftoc = function() {
+const ftoc = function(fahr) {
 
+  const cels = (fahr-32) *(5/9);
+
+  return Math.round(cels * 10) / 10;
 };
 
-const ctof = function() {
+const ctof = function(cels) {
 
+  const fahr = (cels * (9/5)) +32;
+
+  return Math.round(fahr * 10) / 10;
 };
 
 // Do not edit below this line
