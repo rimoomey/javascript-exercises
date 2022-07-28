5f672b898764fbe0ce573c3031941867f1835a55
Rimon Moomey <rimon.moomey@gmail.com>
2022-07-28 15:26:01 -0500

    Complete exercise 2.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(input, numRepetitions) {
 
+    if(numRepetitions < 0) return 'ERROR';
+
+    let outputString = '';
+    for(let i = 0; i < numRepetitions; i++) {
+
+        outputString = outputString.concat(input);
+    }
+
+    return outputString;
 };
 
 // Do not edit below this line
