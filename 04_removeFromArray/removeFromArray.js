6302464289115878a55fbd3136e34615878bfd0e
Rimon Moomey <rimon.moomey@gmail.com>
2022-07-28 16:19:02 -0500

    Complete exercise 4.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,20 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr) {
 
+    const to_array = Array.from(arguments);
+
+    const args = Array.from(to_array.slice(1));
+
+    for(let j = 0; j < args.length; j++) {
+
+        index = arr.indexOf(args[j]);
+
+        if(index === -1) continue;
+
+        arr.splice(index, 1);
+        console.log(arr);
+
+    }
+    return arr;
 };
 
 // Do not edit below this line
