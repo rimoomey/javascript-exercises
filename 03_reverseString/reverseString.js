c5652ac1210e1a727a466a6d253da1fbb03f6317
Rimon Moomey <rimon.moomey@gmail.com>
2022-07-28 15:52:04 -0500

    Complete exercise 3.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,18 @@
-const reverseString = function() {
+const reverseString = function(word) {
 
+    let splitString = word.split("");
+
+    splitString = splitString.reverse();
+
+    let reversed = '';
+
+    for(const char of splitString) {
+
+        reversed = reversed.concat(char);
+        console.log(reversed);
+    }
+
+    return reversed;
 };
 
 // Do not edit below this line
