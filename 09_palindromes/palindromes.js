4c2eb51508eadcadde5bbcd06ff3c6026199e4a5
Rimon Moomey <rimon.moomey@gmail.com>
2022-08-02 14:45:41 -0500

    Complete exercise 9.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,25 @@
-const palindromes = function () {
+const palindromes = function (word) {
 
+    //get rid of punctuation
+    var punctuation = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~ s';
+    let words = word.split('');
+
+    var wordsWeWant = words.filter(function(letter) {
+        return punctuation.indexOf(letter) === -1;
+    })
+
+    words = wordsWeWant.join('');
+    words = words.toUpperCase();
+
+    
+    while(words.length > 1) {
+
+        if(!(words[0] === words[words.length-1])) return false;
+
+        words = words.slice(1,words.length-1);
+    }
+
+    return true;
 };
 
 // Do not edit below this line
