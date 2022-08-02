ca086cbbc9045632ae3b52fe8f308d8fa477d40d
Rimon Moomey <rimon.moomey@gmail.com>
2022-08-02 14:58:15 -0500

    Complete exercise using fibonacci formula.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
 
+    n = parseInt(n);
+    if(!Number.isInteger(n)) return -1;
+    if(n < 0) return 'OOPS';
+
+    let phi = 1.618033988749895;
+
+    return Math.floor(
+
+        Math.round((phi**n - (1-phi)**n)/Math.sqrt(5))
+    )
 };
 
 // Do not edit below this line
