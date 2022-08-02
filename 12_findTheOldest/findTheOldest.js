209f246a0d52dda40b9ced9d680b7d16808fb7d2
Rimon Moomey <rimon.moomey@gmail.com>
2022-08-02 16:27:24 -0500

    Complete exercise 12 with reduce().

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,33 @@
-const findTheOldest = function() {
+const findTheOldest = function(arrPeeps) {
 
+    oldest = arrPeeps.reduce(function(oldest, nextPerson) {
+
+        let age = 0;
+        let oldestAge = 0;
+
+        console.log(oldest);
+
+        if(!nextPerson['yearOfDeath'])
+        {
+            age = (new Date()).getFullYear() - nextPerson['yearOfBirth'];
+        }
+        else age = nextPerson['yearOfDeath'] - nextPerson['yearOfBirth'];
+
+        if(!oldest['yearOfDeath'])
+        {
+            oldestAge = (new Date()).getFullYear() - oldest['yearOfBirth'];
+        }
+        else {
+
+            oldestAge = oldest['yearOfDeath'] - oldest['yearOfBirth'];
+        }
+        if(age > oldestAge) return nextPerson;
+
+        return oldest;
+
+    }, {name: 'noOne', yearOfBirth: 1, yearOfDeath: 1})
+
+    return oldest;
 };
 
 // Do not edit below this line
